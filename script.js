9f5bfa1f83e1e4f4bf31ffe8d9559e4ce9ccc4cf
Memory Tshipapu <83621239+MemoryTshipapu@users.noreply.github.com>
2021-06-16 20:34:33 +0200

    basicCalculator

---
+++ script.js
@@ -0,0 +1,24 @@
+function basicCalculator(operation,num1,num2) {
+    switch(operation) {
+        case "add":
+            return num1+num2;
+            break;
+        case "substract":
+            return num1-num2;
+            break;
+        case "multiply":
+            return num1*num2;
+            break;
+        case "divide":
+            return num1/num2;
+            break;
+        default:
+            console.log("unknown operation or invalid number ");
+    }
+    
+}
+console.log(basicCalculator("add",50,10));
+console.log(basicCalculator("substract",50,10));
+console.log(basicCalculator("divide",50,10));
+console.log(basicCalculator("multiply",50,10));
+console.log(basicCalculator("testError",7,8));
